1d8e9735b0022a842d5e07f23d924c15fe391aff
Desta Equar <154808732+Desta21m@users.noreply.github.com>
2024-12-01 15:51:44 +0300

    setup spriing boot web app

---
+++ src/main/java/com/gommista/bussinessIncomeControle/BussinessIncomeControleApplication.java
@@ -0,0 +1,13 @@
+package com.gommista.bussinessIncomeControle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussinessIncomeControleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BussinessIncomeControleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gommista/bussinessIncomeControle/BussinessIncomeControleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gommista.bussinessIncomeControle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BussinessIncomeControleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
